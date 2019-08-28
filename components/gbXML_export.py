f734d78817d8c90d9b417a1c39cdfdf7f0bf546c
Sebastian <sebastiandmeier@gmail.com>
2019-08-28 17:24:24 +0200

    finished gbXML export component

--- components/gbXML_export.py
+++ components/gbXML_export.py
@@ -13,13 +13,15 @@ room_polylines = [ghc.JoinCurves(segments, False) for segments in room_segments]
 
 
 def brep_area(brep, plane):
+    """ Computes plane area of a brep"""
     section_curve = ghc.BrepXPlane(brep,plane)[0]
     area = ghc.Area(section_curve)[0]
     return area
 
-def get_spatial_data(rooms):
+def get_spatial_data(breps):
+    """ Creates a dictionary with volume and area of the breps"""
     spatial_data = {"area":[],"volume":[]}
-    for i, room_brep in enumerate(rooms):
+    for i, room_brep in enumerate(breps):
         # Get volume
         room_volume, room_centroid = ghc.Volume(room_brep)
         spatial_data["volume"].append(int(room_volume))
@@ -27,47 +29,67 @@ def get_spatial_data(rooms):
         room_area = brep_area(room_brep, ghc.XYPlane(room_centroid))
         spatial_data["area"].append(int(room_area))
     return spatial_data
-spatial_data = get_spatial_data(rooms)
 
-
-
-polyloops = []
-for room_brep in rooms:
-    faces = ghc.DeconstructBrep(room_brep)[0]
+def brep_polyloops(brep):
+    """ Returns the polyloop of all faces for a brep"""
+    faces = ghc.DeconstructBrep(brep)[0]
+    polyloops = []
     for face in faces:
         edges = ghc.DeconstructBrep(face)[1]
         polyline = ghc.JoinCurves(edges, False)
-        vertices = ghc.Explode(polyline)[1]
-        a.append(vertices)
+        vertices = ghc.Explode(polyline, recursive=True)[1]
+        polyloops.append(vertices)
+    return polyloops
 
-
-gbXML_txt = '''<Campus id="cmps-1">
-    <Building id="bldg-1" buildingType="Office">
-        <Area>15811.390974</Area>
-        <Space id="sp-1-Open" zoneIdRef="zone-Default">
-            <Name>1 Open</Name>
-            <Area>3997.5</Area>
-            <Volume>44055.8</Volume>
-            <ShellGeometry id="sg-sp-1-Open" unit="Feet">
-                <ClosedShell>
-                    <PolyLoop>
-                        <CartesianPoint>...</CartesianPoint>
-                        <CartesianPoint>...</CartesianPoint>
-                        <CartesianPoint>...</CartesianPoint>
-                        <CartesianPoint>...</CartesianPoint>
-                    </PolyLoop>
-                    <PolyLoop>...</PolyLoop>
-                    <PolyLoop>...</PolyLoop>
-                    <PolyLoop>...</PolyLoop>
-                    <PolyLoop>...</PolyLoop>
-                    <PolyLoop>...</PolyLoop>
-                </ClosedShell>
-                <AnalyticalShell>...</AnalyticalShell>
+def room_xml_string(room_brep, i):
+    # Header for string concatenation
+    XML_room_header = '''\n        <Space id="sp-%i-Open" zoneIdRef="zone-Default">
+            <Name>%i Open</Name>
+            <Area>%i</Area>
+            <Volume>%i</Volume>
+            <ShellGeometry id="sg-sp-%i-Open" unit="mm">
+                <ClosedShell>''' %(i,i,spatial_data["area"][i],spatial_data["volume"][i],i)
+    
+    # Goemetry strings for string concatenation
+    XML_room_polyloops = ""
+    polyloops = brep_polyloops(room_brep)
+    for polyloop in polyloops:
+        XML_room_polyloops += "\n\t\t\t\t\t<PolyLoop>"
+        for point in polyloop:
+            XML_room_polyloops += "\n"+"\t"*6+"<CartesianPoint>"
+            for coordinate in point:
+                XML_room_polyloops += "\n"+"\t"*7+"<Coordinate>%i<Coordinate>" %int(coordinate)
+            XML_room_polyloops += "\n"+"\t"*6+"</CartesianPoint>"
+        XML_room_polyloops += "\n\t\t\t\t\t</PolyLoop>"
+    
+    # Footer for string concatenation
+    XML_room_footer = '''</ClosedShell>
+                <AnalyticalShell>
+                </AnalyticalShell>
             </ShellGeometry>
-        </Space>
-    </Building>
-</Campus>'''
+        </Space>'''
+    return XML_room_header + XML_room_polyloops + XML_room_footer
 
 
-# Convert outputs to trees
-a = ght.list_to_tree(a)
+# Open file and create list to write
+xml = open(path, "w")
+
+# Get volume and area
+spatial_data = get_spatial_data(rooms)
+
+# Write header and open tags
+XML_header = '''<Campus id="cmps-1">
+    <Building id="bldg-1" buildingType="Office">
+        <Area>%i</Area>''' % sum(spatial_data["area"])
+xml.write(XML_header)
+
+# Write rooms
+for i, room_brep in enumerate(rooms):
+    xml.write(room_xml_string(room_brep, i))
+
+# Write Close tags
+XML_footer = '''</Building>\n</Campus>'''
+xml.write(XML_footer)
+
+# close file
+xml.close()
